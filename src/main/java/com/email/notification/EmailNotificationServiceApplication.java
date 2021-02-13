998aae45b6f7bbd384c71db66cf12a0827c0b2d0
Rakesh kumar <36419224+JavastackspaceIT@users.noreply.github.com>
2021-02-14 04:28:23 +0530

    Update EmailNotificationServiceApplication.java
    Added bootstrap description in main method

--- src/main/java/com/email/notification/EmailNotificationServiceApplication.java
+++ src/main/java/com/email/notification/EmailNotificationServiceApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class EmailNotificationServiceApplication {
-
+//Main method is used for bootstrap the application 
 	public static void main(String[] args) {
 		SpringApplication.run(EmailNotificationServiceApplication.class, args);
 	}
